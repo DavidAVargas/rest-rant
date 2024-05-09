703339e05788c789e868db0429ebf7806a962f6b
David A Vargas <160079694+DavidAVargas@users.noreply.github.com>
2024-05-08 22:56:27 -0500

    adding view with mock data

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,23 @@
-const router = require('express').Router()
+const express = require('express');
+const router = express.Router();
 
+// GET /places
 router.get('/', (req, res) => {
-    res.send('GET /places')
-})
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+    }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+    }];
 
-module.exports = router
+    res.render('places/index', { places });
+});
+
+module.exports = router;
