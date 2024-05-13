471bb8508c139d3d9a5518a1664717b8fe9053dd
David A Vargas <160079694+DavidAVargas@users.noreply.github.com>
2024-05-13 16:09:53 -0500

    adding styles to rest-rant

--- views/home.jsx
+++ views/home.jsx
@@ -1,20 +1,19 @@
-const React = require('react')
-const Def = require('./default')
+const React = require('react');
+const Def = require('./default');
 
-function home () {
-    return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-          </main>
-      </Def>
-    )
-  }
-  
-<a href="/places">
-  <button className="btn-primary">Places Page</button>
-</a>
+function Home() {
+  return (
+    <Def>
+      <main>
+        <h1>HOME</h1>
+        <a href="/places">
+          <button className="btn-primary">Places Page</button>
+        </a>
+      </main>
+    </Def>
+  );
+}
 
+module.exports = Home;
 
-module.exports = home
 
