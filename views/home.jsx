ebaeb1c376349dec842171bce4c3e97aadc771c0
David A Vargas <160079694+DavidAVargas@users.noreply.github.com>
2024-05-13 17:46:43 -0500

    adding bootstrap grid to index page

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,13 @@ function Home() {
     <Def>
       <main>
         <h1>HOME</h1>
+        <div>
+        <img src="/images/food-img.jpg" alt="food" />
+        <div>
+          Photo by <a href='https://unsplash.com/@pwign'>Anh Nguyen</a> on <a href='https://unsplash.com/photos/vegetable-and-meat-on-bowl-kcA-c3f_3FE'>Upsplash</a>
+        </div>
+        </div>
+
         <a href="/places">
           <button className="btn-primary">Places Page</button>
         </a>
@@ -15,5 +22,3 @@ function Home() {
 }
 
 module.exports = Home;
-
-
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,23 +1,33 @@
 const React = require('react');
 const Def = require('../default'); // Assuming 'default' is the name of your default component
 
-function index(data) {
-    let placesFormatted = data.places.map((place, index) => (
-        <div key={index}>
-            <h2>{place.name}</h2>
-            <img src={place.pic} alt={place.name} />
+function index (data) {
+    let placesFormatted = data.places.map((place) => {
+      return (
+        <div className="col-sm-6">
+          <h2>{place.name}</h2>
+          <p className='text-center'>
+            {place.cusisines}
+          </p>
+          <img src={place.pic} alt={place.name} />
+          <p className='text-center'>
+            Located in {place.city}, {place.state}
+          </p>
         </div>
-    ));
-
+      )
+    })
     return (
-        <Def>
-            <main>
-                <h1>PLACES INDEX PAGE</h1>
+      <Def>
+          <main>
+              <h1>Places to Rant</h1>
+              <div className="row">
                 {placesFormatted}
-            </main>
-        </Def>
-    );
-}
+              </div>
+          </main>
+      </Def>
+    )
+  }
+  
 
 module.exports = index;
 
