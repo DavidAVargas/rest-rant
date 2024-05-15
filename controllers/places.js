52dedd9a5b40718d2a57318022a26e3b524f1a31
David A Vargas <160079694+DavidAVargas@users.noreply.github.com>
2024-05-14 21:24:54 -0500

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,35 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+// Import the places model part 5, part 6, step 4
+const places = require('../models/places.js');
+
 // GET /places
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/rest.jpg'
-    }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/code-cat-cafe.jpg'
-    }];
-
     res.render('places/index', { places });
 });
 
 // new from part 5
 router.get('/new', (req, res) => {
-    res.render('places/new')
-  })
-  
-  router.post('/', (req, res) => {
-    
-    console.log(req.body)
-    res.send('POST /places')
-  })
-  
+    res.render('places/new');
+});
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    if (!req.body.pic) {
+        // Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+        req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+        req.body.state = 'USA'
+      }
+      places.push(req.body)
+    res.redirect('/places');
+});
 
 module.exports = router;
