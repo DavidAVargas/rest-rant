ffac4d12eaed50fa6ca04a243000faf3d4d46d49
David A Vargas <160079694+DavidAVargas@users.noreply.github.com>
2024-05-14 20:35:37 -0500

    jazzed up form page

--- views/home.jsx
+++ views/home.jsx
@@ -7,7 +7,7 @@ function Home() {
       <main>
         <h1>HOME</h1>
         <div>
-        <img src="/images/food-img.jpg" alt="food" />
+        <img src='/images/food-img.jpg' alt="food" />
         <div>
           Photo by <a href='https://unsplash.com/@pwign'>Anh Nguyen</a> on <a href='https://unsplash.com/photos/vegetable-and-meat-on-bowl-kcA-c3f_3FE'>Upsplash</a>
         </div>
