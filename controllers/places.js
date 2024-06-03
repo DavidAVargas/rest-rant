2028d9d93a6086619e44ec6b491e012fd82ddb31
David A Vargas <160079694+DavidAVargas@users.noreply.github.com>
2024-06-03 13:35:21 -0500

    part 10 part 3

--- controllers/places.js
+++ controllers/places.js
@@ -47,7 +47,9 @@ router.get('/new', (req, res) => {
 router.get('/:id', (req, res) => {
   // res.send('GET /places/:id stub')
   db.Place.findById(req.params.id)
+  .populate('comments')
     .then(place => {
+      console.log (place.comments)
       res.render('places/show', { place })
     })
     .catch(err => {
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -3,6 +3,34 @@ const Def = require('../default')
 
 function show(data) {
   console.log('data in show', data.id)
+  let comments = (
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
+  )
+  let rating = (
+    <h3 className="inactive"
+      style={{ color: 'darkgray' }}
+    >
+      Not yet rated!
+    </h3>
+  )
+
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border" key={c._id}>
+          <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <strong>- {c.author}</strong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      );
+    });
+  }
+
   return (
     <Def>
       <main>
@@ -41,6 +69,47 @@ function show(data) {
           <button type="submit" className="btn btn-danger">
             Delete
           </button>
+        </form>
+
+        <h2 style={{ color: 'cornflowerblue' }}>Comments</h2>
+        <div className='section-mid-comments'>
+          {comments}
+        </div>
+
+        <h2 style={{ color: 'cornflowerblue' }}>Leave a Review</h2>
+        <form id="comment-form" action={`/places/${data.id}/comment`} method="POST">
+          <label htmlFor="author"> Author </label>
+          <input
+            type="text"
+            name="author"
+            id="author"
+            className="form-control"
+            style={{ backgroundColor: 'white', color: 'black', borderRadius: '6px', margin: '10px' }}
+          />
+          <label htmlFor="content"> Content </label>
+          <input
+            type="textarea"
+            name="content"
+            id="content"
+            className="form-control"
+            style={{ backgroundColor: 'white', color: 'black', borderRadius: '6px', margin: '10px' }}
+          />
+          <label htmlFor="stars"> Star Rating </label>
+          <br />
+          <input type="range" name="stars" min="0" max="5" step="0.5" required />
+          <br />
+          <datalist id="values">
+            <option value="0" label="0"></option>
+            <option value="1" label="1"></option>
+            <option value="2" label="2"></option>
+            <option value="3" label="3"></option>
+            <option value="4" label="4"></option>
+            <option value="5" label="5"></option>
+          </datalist>
+          <label htmlFor="rant"> Rant </label>
+          <br />
+          <input type="checkbox" name="rant" id="rant" />
+          <button type="submit" className="btn btn-outline-primary"> Add Comment </button>
         </form>
 
       </main>
