3d3413cafb5adbd194d985702bf1dcb30042c799
Avi Arga <aviarga@Avis-MacBook-Pro.local>
2024-06-07 17:26:41 -0500

    rest rant edit and delete fix part 11 done

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -74,7 +74,7 @@ function show(data) {
           </div>
         </div>
 
-        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+        <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
           Edit
         </a>
 
